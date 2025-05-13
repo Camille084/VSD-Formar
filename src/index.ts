54837a13d927a1032892dfe135f0fb25a0b132a4
Camille084 <ca08042003@gmail.com>
2025-05-13 23:41:06 +0000

    Camille

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import { mainModule } from "process";
+import sequelize from "sequelize";
 
 const app = express();
 const port = 3000;
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+
+
+main()
+
+async function main() {
+  if (require.main === module) {
+    await sequelize.sync({ force: true }).then( )
+  }
+}
+
+
+
+
